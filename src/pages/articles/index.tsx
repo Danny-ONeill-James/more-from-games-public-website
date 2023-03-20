2dbadf3025d4f2fcab9b45cb7de493a52a5cea15
danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-03-20 13:57:47 +0000

    fix: changed id to string to allow UUID.

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -31,7 +31,7 @@ const Articles: NextPage<IArticlesProps> = ({ articles }) => {
 };
 
 export const getServerSideProps = async () => {
-  const res = await fetch("http://localhost:3000/api/articlesList");
+  const res = await fetch("http://localhost:3001/api/articlesList");
   const data = await res.json();
 
   return {
