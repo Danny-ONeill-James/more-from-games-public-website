e087282290439f66cd7b41a76db35524a7fdd349
danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-04-08 11:45:06 +0100

    fix: miniatures Page

--- src/pages/api/miniaturesList.ts
+++ src/pages/api/miniaturesList.ts
@@ -1,31 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { IMiniature } from "@/utilities/interfaces";
 import miniatures from "@/utilities/data/miniaturesList.json";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<IMiniature[]>
-) {
-  if (req.method !== "POST") {
-    const miniaturesJson = miniatures.Miniatures;
+export async function MiniaturesListApi() {
+  const response = await miniatures.Miniatures;
 
-    res.status(200).json(miniaturesJson);
-  } else {
-    const miniatureJson = await miniatures.Miniatures;
-    const postBody = await JSON.parse(req.body);
-    let foundMiniature: IMiniature[] = new Array();
-
-    miniatureJson.forEach((element) => {
-      if (element.gameLink == postBody) {
-        foundMiniature.push(element);
-      }
-    });
-
-    if (foundMiniature.length == 0) {
-      res.status(200).json(foundMiniature);
-    } else {
-      res.status(200).json(foundMiniature);
-    }
-  }
+  return response;
 }
